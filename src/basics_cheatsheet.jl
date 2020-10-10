078947bb6364774b57da5572da24b9e2fb92e3c9
lourielee <lcmailbox19@gmail.com>
2020-10-10 15:30:55 -0500

    initial commit

---
+++ src/basics_cheatsheet.jl
@@ -0,0 +1,9 @@
+open and print out file line by line:
+
+#1
+in_file = readlines("/Users/lauriecannon/myfile.txt")
+
+#2
+for line in eachline("/Users/lauriecannon/myfile.txt")
+    println(line)
+end
