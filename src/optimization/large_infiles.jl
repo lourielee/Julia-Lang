0862b3ed4f231208723d5711134ec5bfcca1a9cf
lourielee <lauriecannon@gmail.com>
2020-10-22 16:55:12 -0500

    add tasks with async

--- src/optimization/large_infiles.jl
+++ src/optimization/large_infiles.jl
@@ -1,13 +1,23 @@
-#to Optimize:
+#Optimize various Input file functions
+
+#=From outfiles:
+without async: 3.685943941, 3.782235015, 4.004312407
+with @async: 4.61e-6, 3.755e-6, 2.632e-6
+=#
+
 function readLine_bigFile()
-    infile = ("/data/testdata.txt")
-    data = []
-    for line in eachline(infile)
-        push!(data, line)
-    end
+    infile = ("/Users/lauriecannon/projects/Julia/Julia_binf/data/testdata.txt.txt")
+    #add @async for concurrent tasks:
+    @async data = readchomp(infile)
 end
 
 #checks
-outfile = open("/scratch/scratch_output.txt", "w")
-println(outfile, @elapsed check())
-println(outfile, "\n\noutput @elapsed for 20 mil lines") #~20-30 seconds
+outfile = open("/Users/lauriecannon/projects/Julia/Julia_binf/scratch/large_infiles_tasks.output", "w")
+println(outfile, @elapsed readLine_bigFile())
+println(outfile, @elapsed readLine_bigFile())
+println(outfile, @elapsed readLine_bigFile())
+
+
+#tasks/threads for:
+    #replace(line, "1" => "A")
+    #push!(data, line)
