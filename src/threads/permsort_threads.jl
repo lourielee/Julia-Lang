f26b5a25f2cc940474b2c7fdb6b2d349473bed41
lourielee <lauriecannon@gmail.com>
2020-10-22 14:51:09 -0500

    directory

--- src/threads/permsort_threads.jl
+++
@@ -1,7 +0,0 @@
-#tryout Julia multithreading: make paramsort faster
-
-using Random
-
-JULIA_NUM_THREADS = 2 #not supported todo if/else ARGS, or open shell to set threads
-println(Threads.nthreads())
-#arr = rand(100000000) #readability: 100 million
