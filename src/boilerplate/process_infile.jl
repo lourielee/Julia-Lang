44a76d729f728be684beba8cadf2d16029849c71
lourielee <lauriecannon@gmail.com>
2020-10-24 16:16:32 -0500

    add function calls

--- src/boilerplate/process_infile.jl
+++ src/boilerplate/process_infile.jl
@@ -1,5 +1,3 @@
-#for various common file formats
-
 function linesToArray(file)
     infile = file
     data = []
@@ -9,11 +7,27 @@ function linesToArray(file)
     return data
 end
 
+#removes \n and spaces
 function wordsToArray(file)
     infile = file
-    data = []
-    for line in eachline(infile)
-        push!(data, split(line, " "))
-    end
-    return data
+    data = readchomp(infile)
+    data = replace(data, "\n" => " ")
+    return split(data, " ")
+end
+
+#removes new lines
+function tokensToArray(file)
+    infile = file
+    data = readchomp(infile)
+    data = replace(data, "\n" => " ")
+    return split(data, "")
+end
+
+#remove all white space
+function tokens_strip_toArray(file)
+    infile = file
+    data = readchomp(infile)
+    data = replace(data, "\n" => " ")
+    data = replace(data, " " => "")
+    return split(data, "")
 end
